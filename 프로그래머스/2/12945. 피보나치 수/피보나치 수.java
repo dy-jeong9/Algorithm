02e4ddaac7c3b358b55204150708bcaa4f230244
dy-jeong9 <155612470+dy-jeong9@users.noreply.github.com>
2024-03-13 02:54:43 +0900

    [level 2] Title: 피보나치 수, Time: 3.10 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -4,12 +4,11 @@ class Solution {
     public int solution(int n) {
         
         //피보나치 수 담을 배열 arr
-        //n번까지의 arr 배열의 길이 선언
-        //arr의 길이 다시 선언
+        //arr의 길이
         //for문으로 arr 인자 만들어 arr에 담기
         //n번째 인자 나누기 & 리턴
         
-        // double[] arr = {0,1,1,2,3,5};
+        // int[] arr = {0,1,1,2,3,5};
         int answer = 0;
         if(2<=n && n<=100000){
             int leng = n + 1;
@@ -18,14 +17,13 @@ class Solution {
             arr[1] = 1;
 
             for(int i=2; i<leng; i++){
+                //(A + B) % C = ( ( A % C ) + ( B % C) ) % C
                 int f = arr[i-1] + arr[i-2];
                 arr[i] = f % 1234567;
             }
 
             answer = arr[n];
 
-            // System.out.println(arr[n]);
-            // System.out.println(answer);
         }
         
         return answer;
