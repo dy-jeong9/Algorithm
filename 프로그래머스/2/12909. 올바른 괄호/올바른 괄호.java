08e53d43f3178a5968d6fa59e9208b2b02f70f8e
dy-jeong9 <155612470+dy-jeong9@users.noreply.github.com>
2024-03-27 03:28:58 +0900

    [level 2] Title: 올바른 괄호, Time: 31.81 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,22 +2,26 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        //s를 stack에 하나씩 넣으면서 괄호가 짝지어지면 pop
-        //마지막에 stack이 비어 있으면 true, 아니면 false
+        //문자열 s를 조건에 따라 하나씩 stack에 넣는다.
+        //'('이면 stack에 push
+        //')'이고 stack이 비었으면 false
+        //')'이고 stack에 값(='(')이 있으면 pop
+        //stack의 empty 여부
 
         Stack<Character> stack = new Stack<>();
                 
         for(int i = 0 ; i < s.length(); i++){
+            //'('는 stack에 넣는다. 
             if(s.charAt(i)=='('){
                 stack.push(s.charAt(i));
+            //')'일 때
             } else if(s.charAt(i)==')'){
-                if(stack.isEmpty()){
-                    return false;
-                }
+                //stack이 비어 있으면 false
+                if(stack.isEmpty()) return false;
+                //비어있지 않으면 = ( 가 있으면 pop
                 stack.pop();
             }
         }
-
         return stack.isEmpty();
     }
 }
