4cab0323603517aa5a1e5ed4e7f34a64ce8c6b6f
dy-jeong9 <155612470+dy-jeong9@users.noreply.github.com>
2024-03-27 01:43:47 +0900

    Update 과일 장수.java

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,19 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        // score를 sort한다.
-        // 마지막부터 m개째 마다 stack에 담으면 stack에는 각 상자의 가장 낮은 점수의 사과가 담긴다.
-        // stack에 담긴 점수를 사과수 만큼 곱해서 합한다.
-        
         int scoreLength = score.length; // 사과 갯수
-        
         Stack<Integer> stack = new Stack<>();
-        
+
+        // score를 sort한다.
         Arrays.sort(score);
+
+        // 마지막부터 m개째 마다 stack에 담으면 stack에는 각 상자의 가장 낮은 점수의 사과가 담긴다.
         for(int i = scoreLength - m ; i >= 0 ; i-=m){
               stack.push(score[i]);
         }
-        
+
+        // stack에 담긴 점수를 사과수 만큼 곱해서 합한다.
         for(int i = 0; i<scoreLength/m ; i++){
             answer += stack.pop() * m;
         }
