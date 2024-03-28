ac740ff76487071c01945054c4af3594aaca7712
dy-jeong9 <155612470+dy-jeong9@users.noreply.github.com>
2024-03-28 22:37:27 +0900

    Create 주행거리 비교하기.java

---
+++ 소프티어/1/6253. 주행거리 비교하기/주행거리 비교하기.java
@@ -0,0 +1,23 @@
+//https://softeer.ai/practice/6253
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt(); //A의 주행거리
+        int B = sc.nextInt(); //B의 주행거리
+
+        String answer = "";
+        if(A == B){
+            answer = "same";
+        } else {
+            answer = (A > B)? "A" : "B";
+        }
+
+        System.out.println(answer);
+        
+    }
+}
