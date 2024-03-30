c2ae8f6da16f8e2dc726a291d4b5697b27b1963c
dy-jeong9 <155612470+dy-jeong9@users.noreply.github.com>
2024-03-30 23:30:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.70 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        //참가자, 완주자 목록에서 모두 짝수번 등장해야 완주
+        //참가자와 완주자를 순차적으로 map에 먼저 넣고, 등장할 때마다 true, false 업데이트
+        //마지막에 true 값을 갖는 사람(key) 리턴
+        
+        Map<String, Boolean> marathon = new HashMap<>();
+        
+        for(int i = 0 ; i < participant.length ; i++){
+            if(marathon.containsKey(participant[i])){
+                marathon.remove(participant[i]);
+            } else {
+                marathon.put(participant[i], true);
+            }
+        }
+        
+        for(int i = 0 ; i < completion.length ; i++){
+            if(marathon.containsKey(completion[i])){
+                marathon.remove(completion[i]);
+            } else {
+                marathon.put(completion[i], true);
+            }
+        }
+        
+        for (String key : marathon.keySet()) {
+            //System.out.println(key);
+            answer = key;
+            break;
+        }
+
+        
+        return answer;
+    }
+}
