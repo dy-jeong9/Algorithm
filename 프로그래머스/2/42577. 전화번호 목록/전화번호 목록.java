a4120c4683a8a0e37f83dd49002e91de6b838c37
dy-jeong9 <155612470+dy-jeong9@users.noreply.github.com>
2024-03-31 20:56:30 +0900

    [level 2] Title: 전화번호 목록, Time: 569.56 ms, Memory: 199 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        //phone_book의 인자를 모두 key로, value는 true 저장 //key만 저장하면됨.
+        //key가 phone_book의 전화번호의 substring을 포함하는지 확인.
+        //포함한다면 false 리턴
+        
+        Map<String, Boolean> keyNum = new HashMap<>(); //phone_book의 key를 담을 map
+        
+        for(String key : phone_book){
+            keyNum.put(key, true);
+        }
+        
+        for(String phoneNum : phone_book){
+            for(int i = 0; i < phoneNum.length(); i++){
+                if(keyNum.containsKey(phoneNum.substring(0, i))){
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
