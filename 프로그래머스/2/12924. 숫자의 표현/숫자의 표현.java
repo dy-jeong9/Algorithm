db79288bce620932a77060a87dd610e9e7fe1fba
dy-jeong9 <155612470+dy-jeong9@users.noreply.github.com>
2024-04-03 14:22:41 +0900

    [level 2] Title: 숫자의 표현, Time: 0.09 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,16 +1,8 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        // n = 짝수 : 모든 약수 개수 / n =홀수 => 홀수 약수 개수
-        
-        for(int i = 1 ; i <= n ; i++){
-            if(n%i == 0){
-                if(n%2 == 0){//짝수
-                    if(i%2 == 1) answer++; //홀수면 +1
-                } else {//홀수
-                    answer++; //약수면 +1
-                }
-            }
+        for(int i = 1 ; i <= n ; i += 2){ // 홀수만 확인
+            if(n % i == 0) answer++;
         }
         return answer;
     }
