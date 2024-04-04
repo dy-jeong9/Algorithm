676e901155d0e0534d5d5655ba011a29ee67e719
dy-jeong9 <155612470+dy-jeong9@users.noreply.github.com>
2024-04-04 23:42:33 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 13.59 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,21 +1,26 @@
 import java.util.*;
 class Solution {
     public long solution(long n) {
+        //n을 10으로 자릿수 만큼 나눠서 배열에 담는다.
+        //배열을 정렬한다.
+        //배열을 역순으로 정렬한다.
+        //Long타입으로 리턴한다.
         long answer = 0;
-        String N = n + ""; //n을 String 타입으로 만들어준다
-        String[] arrN = N.split(""); //N을 배열에 담는다
+        String N = n + ""; //n의 길이를 사용하기 위해 string 타입으로 변환
+        long[] arrayN = new long[N.length()]; //N을 담을 배열
         
-        String reverse = ""; //큰것부터 작은 순으로 정렬한 새로운 문자열
+        String k = "";
         
-        Arrays.sort(arrN); // 오름차순 정렬
-        
-        for(int i = N.length()-1; i >= 0 ; i--){ //내림차순 해주기 위한 반복문
-            reverse += arrN[i];
+        for(int i = 0 ; i < N.length() ; i++){
+            arrayN[i] = n % (long)10;
+            n /= 10;
         }
-
-        answer = Long.parseLong(reverse); // Long타입으로 변환
+        Arrays.sort(arrayN);
         
-        
+        for(int i = N.length()-1 ; i >= 0 ; i--){
+            k += arrayN[i];
+        }
+        answer = Long.parseLong(k);
         return answer;
     }
 }
